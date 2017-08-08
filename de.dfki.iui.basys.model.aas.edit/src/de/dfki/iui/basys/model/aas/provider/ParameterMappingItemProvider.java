2018d4e48b8260f2b9f87ac557663e4cf670f622
Daniel Porta <daniel.porta@dfki.de>
2017-08-08 16:00:18 +0200

    missing @generated NOT added

--- de.dfki.iui.basys.model.aas.edit/src/de/dfki/iui/basys/model/aas/provider/ParameterMappingItemProvider.java
+++ de.dfki.iui.basys.model.aas.edit/src/de/dfki/iui/basys/model/aas/provider/ParameterMappingItemProvider.java
@@ -125,7 +125,7 @@ public class ParameterMappingItemProvider
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public String getText(Object object) {
