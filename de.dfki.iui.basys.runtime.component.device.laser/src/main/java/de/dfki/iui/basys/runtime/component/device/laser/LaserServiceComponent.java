09d93abc8769f7517117c7058203346a7178879c
Frederic Kerber <frederic.kerber@dfki.de>
2018-05-17 11:50:20 +0200

    Path handling for Laser implemented. Preparations for integration of dynamic scheduling component.

--- de.dfki.iui.basys.runtime.component.device.laser/src/main/java/de/dfki/iui/basys/runtime/component/device/laser/LaserServiceComponent.java
+++ de.dfki.iui.basys.runtime.component.device.laser/src/main/java/de/dfki/iui/basys/runtime/component/device/laser/LaserServiceComponent.java
@@ -1,5 +1,10 @@
 package de.dfki.iui.basys.runtime.component.device.laser;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +19,7 @@ import de.dfki.iui.basys.common.emf.json.JsonUtils;
 import de.dfki.iui.basys.model.data.CartesianCoordinate;
 import de.dfki.iui.basys.model.data.DataPackage;
 import de.dfki.iui.basys.model.data.Path;
+import de.dfki.iui.basys.model.data.impl.CartesianCoordinateImpl;
 import de.dfki.iui.basys.model.domain.capability.CapabilityFactory;
 import de.dfki.iui.basys.model.domain.capability.ProjectETA;
 import de.dfki.iui.basys.model.domain.capability.ProjectPath;
@@ -52,6 +58,146 @@ public class LaserServiceComponent extends DeviceControllerServiceComponent {
 				}
 			}
 		}
+	}
+
+	private static double getDistance(CartesianCoordinate cc1, CartesianCoordinate cc2) {
+		return Math.sqrt(Math.pow((cc2.getX() - cc1.getX()), 2) + Math.pow((cc2.getY() - cc1.getY()), 2)
+				+ Math.pow((cc2.getZ() - cc1.getZ()), 2));
+	}
+
+	private static double getLength(CartesianCoordinate cc) {
+		return Math.sqrt(Math.pow(cc.getX(), 2) + Math.pow(cc.getY(), 2) + Math.pow(cc.getZ(), 2));
+	}
+
+	private static CartesianCoordinate getDirectionVector(CartesianCoordinate cc1, CartesianCoordinate cc2) {
+
+		return new CartesianCoordinateImpl.Builder().x(cc2.getX() - cc1.getX()).y(cc2.getY() - cc1.getY())
+				.z(cc2.getZ() - cc1.getZ()).build();
+	}
+
+	private static CartesianCoordinate norm(CartesianCoordinate cc) {
+		double length = getLength(cc);
+		return new CartesianCoordinateImpl.Builder().x(cc.getX() / length).y(cc.getY() / length).z(cc.getZ() / length)
+				.build();
+	}
+
+	private static CartesianCoordinate scalarProduct(CartesianCoordinate cc, double scalar) {
+		return new CartesianCoordinateImpl.Builder().x(cc.getX() * scalar).y(cc.getY() * scalar).z(cc.getZ() * scalar)
+				.build();
+	}
+
+	private static CartesianCoordinate add(CartesianCoordinate cc1, CartesianCoordinate cc2) {
+		return new CartesianCoordinateImpl.Builder().x(cc1.getX() + cc2.getX()).y(cc1.getY() + cc2.getY())
+				.z(cc1.getZ() + cc2.getZ()).build();
+	}
+
+	private static List<CartesianCoordinate> beautifyPath(List<CartesianCoordinate> coords, CartesianCoordinate currentPosition, double pathLengthToCover,
+			double interArrowDistance) {
+		
+		if (coords.size() < 2) {
+			return coords;
+		}
+		List<CartesianCoordinate> result = new ArrayList<CartesianCoordinate>();
+		result.add(coords.get(0));
+
+		double currentDesiredDistance = interArrowDistance;
+
+		for (int x = 1; x < coords.size(); x++) {
+
+			CartesianCoordinate cc1 = coords.get(x - 1);
+			CartesianCoordinate cc2 = coords.get(x);
+
+			double remainingSegmentLength = getDistance(cc1, cc2);
+			CartesianCoordinate normedSegmentDirectionVector = norm(getDirectionVector(cc1, cc2));
+
+			CartesianCoordinate baseCoordinate = cc1;
+
+			while (remainingSegmentLength > currentDesiredDistance && pathLengthToCover > 0) {
+
+				baseCoordinate = add(baseCoordinate, scalarProduct(normedSegmentDirectionVector, currentDesiredDistance));
+				result.add(baseCoordinate);
+				remainingSegmentLength -= currentDesiredDistance;
+				pathLengthToCover -= interArrowDistance;
+				currentDesiredDistance = interArrowDistance;
+			}
+			currentDesiredDistance -= remainingSegmentLength;
+		}
+	
+		
+		return result;
+	}
+
+	public static void main(String[] args) {
+		List<CartesianCoordinate> input = new ArrayList<>();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("/Users/fkerber/Downloads/MIR100.csv"));
+			String str = null;
+			while ((str = br.readLine()) != null) {
+				String[] spl = str.split(";");
+
+				input.add(new CartesianCoordinateImpl.Builder().x(Double.parseDouble(spl[0]))
+						.y(Double.parseDouble(spl[1])).z(0).build());
+			}
+			br.close();
+
+			CartesianCoordinate mir1 = new CartesianCoordinateImpl.Builder().x(1.27)
+					.y(4.03).z(0).build();
+			
+			
+			
+			
+			BufferedWriter bw = new BufferedWriter(new FileWriter("/Users/fkerber/Downloads/MIR100_beauty.csv"));
+			for (CartesianCoordinate cc : beautifyPath(input, mir1, 15, .1)) {
+				String xString = cc.getX() + "";
+				String yString = cc.getY() + "";
+				xString = xString.replace(".", ",");
+				yString = yString.replace(".", ",");
+				bw.write(xString + ";" + yString + "\n");
+			}
+			bw.close();
+			
+			CartesianCoordinate mir2 = new CartesianCoordinateImpl.Builder().x(1.238023998)
+					.y(3.93520143).z(0).build();
+			
+			if (getDistance(mir2, input.get(0))< 0.11) {
+				input.remove(0);
+			}
+			input.add(0, mir2);
+			
+			bw = new BufferedWriter(new FileWriter("/Users/fkerber/Downloads/MIR100_2_beauty.csv"));
+			for (CartesianCoordinate cc : beautifyPath(input, mir2, 15, .1)) {
+				String xString = cc.getX() + "";
+				String yString = cc.getY() + "";
+				xString = xString.replace(".", ",");
+				yString = yString.replace(".", ",");
+				bw.write(xString + ";" + yString + "\n");
+			}
+			bw.close();
+			
+			
+			CartesianCoordinate mir3 = new CartesianCoordinateImpl.Builder().x(1.206047997)
+					.y(3.840500287).z(0).build();
+			
+			input.remove(0);
+			if (getDistance(mir3, input.get(0))< 0.11) {
+				input.remove(0);
+			}
+			input.add(0, mir3);
+			
+			bw = new BufferedWriter(new FileWriter("/Users/fkerber/Downloads/MIR100_3_beauty.csv"));
+			for (CartesianCoordinate cc : beautifyPath(input, mir3, 15, .1)) {
+				String xString = cc.getX() + "";
+				String yString = cc.getY() + "";
+				xString = xString.replace(".", ",");
+				yString = yString.replace(".", ",");
+				bw.write(xString + ";" + yString + "\n");
+			}
+			bw.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
