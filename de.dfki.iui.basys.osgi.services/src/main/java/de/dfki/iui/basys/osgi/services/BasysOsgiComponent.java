3b0d3023a8806859d2be0ea9b0c813be72fca9e9
Daniel Porta <daniel.porta@dfki.de>
2018-05-17 22:27:38 +0200

    properties to component config

--- de.dfki.iui.basys.osgi.services/src/main/java/de/dfki/iui/basys/osgi/services/BasysOsgiComponent.java
+++ de.dfki.iui.basys.osgi.services/src/main/java/de/dfki/iui/basys/osgi/services/BasysOsgiComponent.java
@@ -1,16 +1,12 @@
 package de.dfki.iui.basys.osgi.services;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.dataformat.javaprop.JavaPropsMapper;
-
+import de.dfki.iui.basys.model.runtime.component.ComponentCategory;
 import de.dfki.iui.basys.model.runtime.component.ComponentConfiguration;
 import de.dfki.iui.basys.model.runtime.component.impl.ComponentConfigurationImpl;
 
@@ -35,27 +31,24 @@ public class BasysOsgiComponent {
 	}
 
 	protected ComponentConfiguration propsToComponentConfig(Map<String, Object> properties) {
-		ComponentConfiguration config = null;
-		try {
 
-			// SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.serializeAllExcept("component", "component.id", "component.name", "service.pid", "service");
-			// FilterProvider filterProvider = new SimpleFilterProvider().addFilter("component", filter);
+		//Map<String, Object> props = new HashMap<String, Object>(properties);
+		//props.remove("component.id");
+		//props.remove("component.name");
+		//props.remove("service.pid");
+		
+		ComponentConfiguration config = new ComponentConfigurationImpl.Builder()
+			.componentId((String)properties.get("componentId"))
+			.componentName((String)properties.get("componentName"))
+			.componentCategory(ComponentCategory.get((String)properties.get("componentCategory")))
+			.componentImplementationJavaClass((String)properties.get("componentImplementationJavaClass"))
+			.communicationProviderImplementationJavaClass((String)properties.get("communicationProviderImplementationJavaClass"))
+			.communicationProviderConnectionString((String)properties.get("communicationProviderConnectionString"))
+			.inChannelName((String)properties.get("inChannelName"))
+			.outChannelName((String)properties.get("outChannelName"))
+			.externalConnectionString((String)properties.get("externalConnectionString"))
+			.build();
 
-			Map<String, Object> props = new HashMap<String, Object>(properties);
-			props.remove("component.id");
-			props.remove("component.name");
-			props.remove("service.pid");
-
-			JavaPropsMapper mapper = new JavaPropsMapper();
-			String asText = mapper.writeValueAsString(props);
-			config = mapper.readValue(asText, ComponentConfigurationImpl.class);
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		return config;
 	}
 
