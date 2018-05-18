73c1295655607b565f72602b6d2f0f4689342e7e
sokn01 <sokn01@nb-647.dfki.net>
2018-05-18 14:09:47 +0200

    added switch device component

---
+++ de.dfki.iui.basys.runtime.component.device.mqtt/src/main/java/de/dfki/iui/basys/runtime/component/device/mqtt/tactileswitch/SwitchDeviceComponent.java
@@ -0,0 +1,170 @@
+package de.dfki.iui.basys.runtime.component.device.mqtt.tactileswitch;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import de.dfki.iui.basys.model.domain.capability.Capability;
+import de.dfki.iui.basys.model.domain.capability.CapabilityPackage;
+import de.dfki.iui.basys.model.domain.capability.SwitchConfirmationCapability;
+import de.dfki.iui.basys.model.runtime.component.CapabilityRequest;
+import de.dfki.iui.basys.model.runtime.component.ComponentConfiguration;
+import de.dfki.iui.basys.runtime.component.ComponentContext;
+import de.dfki.iui.basys.runtime.component.ComponentException;
+import de.dfki.iui.basys.runtime.component.device.mqtt.MqttDeviceComponent;
+import de.dfki.iui.basys.runtime.component.device.packml.UnitConfiguration;
+
+public class SwitchDeviceComponent extends MqttDeviceComponent {
+
+	private final Integer qos = 0;
+	private final String topic = "switch";
+	
+	private Object lock = new Object();
+	
+	CountDownLatch counter;
+	
+	public SwitchDeviceComponent(ComponentConfiguration config) {
+		super(config);
+		resetOnComplete = true;
+		resetOnStopped = true;
+	}
+	
+	@Override
+	protected UnitConfiguration translateCapabilityRequest(CapabilityRequest req) {
+
+		Capability c = (Capability) req.getCapability();
+
+		SwitchConfirmationCapability switchConfirmationCapability = null;
+		if (c.eClass().equals(CapabilityPackage.eINSTANCE.getSwitchConfirmationCapability()))
+			switchConfirmationCapability = (SwitchConfirmationCapability) c;
+
+		UnitConfiguration uc = new UnitConfiguration();
+		if (switchConfirmationCapability!=null)
+			uc.setPayload(switchConfirmationCapability.getState());
+		
+		return uc;
+
+	}
+	
+	@Override
+	public void activate(ComponentContext context) throws ComponentException {
+		super.activate(context);
+		
+	}
+	
+
+	@Override
+	public void onStarting() {
+		
+		UnitConfiguration unitConfiguration = getUnitConfig();
+		int state = (int) unitConfiguration.getPayload();
+		counter = new CountDownLatch(1);
+		
+		
+		if (state==1)
+			switchActivate();
+		else if (state==0)
+			switchDeactivate();
+	}
+	
+	@Override
+	public void onExecute() {
+		
+			try {
+				counter.await();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			LOGGER.info("EXECUTE FINISHED");
+		
+	}
+
+	@Override
+	public void onCompleting() {
+		switchDeactivate();
+		
+		//TODO: notify Basys
+		//outChannel.sendMessage("");
+	}
+	
+	
+	@Override
+	public void onResetting() {
+		// TODO Auto-generated method stub
+		super.onResetting();
+	}
+	
+	private boolean switchActivate() {
+
+		try {
+			mqttClient.subscribe(topic, qos, new IMqttMessageListener() {
+
+				@Override
+				public void messageArrived(String topic, MqttMessage message) throws Exception {
+
+					String sMessage = new String(message.getPayload());
+					if (sMessage.contains("Switch") && sMessage.contains("Event")) {
+						counter.countDown();
+					}
+
+				}
+			}).waitForCompletion();
+		} catch (MqttException e) {
+			e.printStackTrace();
+		}
+		
+		publish(topic, "01");
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		publish(topic, "activate");
+		return true;
+	}
+	
+	private boolean switchDeactivate() {
+	
+		publish(topic, "10");
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		publish(topic, "deactivate");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		publish(topic, "00");
+		
+		try {
+			mqttClient.unsubscribe(topic).waitForCompletion();
+			LOGGER.info("UNSUB");
+		} catch (MqttException e) {
+			e.printStackTrace();
+		}
+		
+		return true;
+	}
+	
+	private void publish(String topic, String content) {
+		final MqttMessage message = new MqttMessage(content.getBytes());
+        message.setQos(qos);
+        try {
+        	mqttClient.publish(topic, message).waitForCompletion();
+        	System.out.printf("publishing message %s on topic %s%n", message, topic);
+        } catch (MqttException e) {
+        	e.printStackTrace();
+        }
+	}
+	
+}
---
+++ de.dfki.iui.basys.runtime.component.device.mqtt/src/test/java/de/dfki/iui/basys/runtime/component/device/mqtt/tactileswitch/SwitchDeviceComponentTest.java
@@ -0,0 +1,220 @@
+package de.dfki.iui.basys.runtime.component.device.mqtt.tactileswitch;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import de.dfki.iui.basys.common.emf.json.JsonUtils;
+import de.dfki.iui.basys.model.data.CartesianCoordinate;
+import de.dfki.iui.basys.model.data.DataFactory;
+import de.dfki.iui.basys.model.data.Path;
+import de.dfki.iui.basys.model.data.impl.CartesianCoordinateImpl;
+import de.dfki.iui.basys.model.domain.capability.CapabilityFactory;
+import de.dfki.iui.basys.model.domain.capability.ProjectETA;
+import de.dfki.iui.basys.model.domain.capability.ProjectPath;
+import de.dfki.iui.basys.model.domain.capability.SwitchConfirmationCapability;
+import de.dfki.iui.basys.model.runtime.communication.Request;
+import de.dfki.iui.basys.model.runtime.component.CapabilityRequest;
+import de.dfki.iui.basys.model.runtime.component.ComponentCategory;
+import de.dfki.iui.basys.model.runtime.component.ComponentConfiguration;
+import de.dfki.iui.basys.model.runtime.component.ComponentFactory;
+import de.dfki.iui.basys.model.runtime.component.State;
+import de.dfki.iui.basys.model.runtime.component.impl.ComponentConfigurationImpl;
+import de.dfki.iui.basys.runtime.communication.ClientFactory;
+import de.dfki.iui.basys.runtime.component.ComponentContext;
+import de.dfki.iui.basys.runtime.component.ComponentException;
+
+public class SwitchDeviceComponentTest {
+
+	ComponentConfiguration componentConfig;
+	ComponentContext emptyContext = new ComponentContext.Builder().build();
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		String basysBroker = "tcp://10.2.10.4:1883";
+		String localBroker = "tcp://127.0.0.1:1883";
+		componentConfig = new ComponentConfigurationImpl.Builder()
+				.componentId("switch-component")
+				.componentName("switch-component")
+				.inChannelName("switch-component#in")
+				.outChannelName("switch-component#out")
+				.componentCategory(ComponentCategory.DEVICE_COMPONENT)
+				.externalConnectionString(localBroker).build();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	private void sleep(long seconds) {
+		try {
+			TimeUnit.MILLISECONDS.sleep(seconds*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testConnection() throws ComponentException {
+		
+		SwitchDeviceComponent component = new SwitchDeviceComponent(componentConfig);
+		assertTrue(!component.isConnectedToExternal());
+
+		component.activate(emptyContext);
+		assertTrue(component.isConnectedToExternal());
+		
+		component.deactivate();		
+		assertTrue(!component.isConnectedToExternal());
+	}
+	
+	
+	
+	@Test
+	public void testActivateStop() throws ComponentException {
+		SwitchDeviceComponent component = new SwitchDeviceComponent(componentConfig);
+		assertTrue(!component.isConnectedToExternal());
+
+		component.activate(emptyContext);
+		assertTrue(component.isConnectedToExternal());
+		
+		
+		component.reset();
+		sleep(1);
+		
+		SwitchConfirmationCapability capability = CapabilityFactory.eINSTANCE.createSwitchConfirmationCapability();
+		capability.setState(1);
+		
+		CapabilityRequest req = ComponentFactory.eINSTANCE.createCapabilityRequest();
+		req.setCapability(capability);
+		req.setComponentId(componentConfig.getComponentId());
+		
+		try {
+			String payload = JsonUtils.toString(req);
+			Request message = ClientFactory.getInstance().createRequest(payload);
+			component.handleRequest(null, message);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		
+		sleep(3);
+				
+		assertEquals(State.EXECUTE, component.getState());
+		
+		component.stop();
+		
+		sleep(3);
+		
+		assertEquals(State.IDLE, component.getState());
+		
+		component.deactivate();		
+		assertTrue(!component.isConnectedToExternal());
+	}
+	
+	
+	
+	@Test
+	public void testActivateComplete() throws ComponentException {
+		SwitchDeviceComponent component = new SwitchDeviceComponent(componentConfig);
+		assertTrue(!component.isConnectedToExternal());
+
+		component.activate(emptyContext);
+		assertTrue(component.isConnectedToExternal());
+		
+		
+		component.reset();
+		sleep(1);
+		
+		SwitchConfirmationCapability capability = CapabilityFactory.eINSTANCE.createSwitchConfirmationCapability();
+		capability.setState(1);
+		
+		CapabilityRequest req = ComponentFactory.eINSTANCE.createCapabilityRequest();
+		req.setCapability(capability);
+		req.setComponentId(componentConfig.getComponentId());
+		
+		try {
+			String payload = JsonUtils.toString(req);
+			Request message = ClientFactory.getInstance().createRequest(payload);
+			component.handleRequest(null, message);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		
+		sleep(3);
+				
+		assertEquals(State.EXECUTE, component.getState());
+		
+		
+		
+		sleep(10);
+		
+		assertEquals(State.IDLE, component.getState());
+		
+		component.deactivate();		
+		assertTrue(!component.isConnectedToExternal());
+	}
+	
+/*
+	@Test
+	public void testEtaProjection() throws ComponentException {
+		LaserDeviceComponent component = new LaserDeviceComponent(componentConfig);
+		assertTrue(!component.isConnectedToExternal());
+
+		component.activate(emptyContext);
+		assertTrue(component.isConnectedToExternal());
+		
+		sleep(1); // für reset()
+		
+		CartesianCoordinate position = new CartesianCoordinateImpl.Builder().x(1.0).y(1.0).z(0.0).build();		
+		
+		ProjectETA capability = CapabilityFactory.eINSTANCE.createProjectETA();
+		capability.setEta(30*1000); // Millisekunden
+		capability.setPosition(position);
+		capability.setRadius(0.25); // Meter
+		capability.setColor(0);
+		
+		CapabilityRequest req = ComponentFactory.eINSTANCE.createCapabilityRequest();
+		req.setCapability(capability);
+		req.setComponentId(componentConfig.getComponentId());
+		
+		try {
+			String payload = JsonUtils.toString(req);
+			Request message = ClientFactory.getInstance().createRequest(payload);
+			component.handleRequest(null, message);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		
+		sleep(3);
+				
+		assertEquals(State.EXECUTE, component.getState());
+		
+		component.stop();
+		
+		sleep(3);
+		
+		assertEquals(State.IDLE, component.getState());
+		
+		component.deactivate();		
+		assertTrue(!component.isConnectedToExternal());
+	}
+*/
+}
